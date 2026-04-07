5145a3b6384d899f8d56c9f20df52e22bfd54adf
Heysoos <sina.abdollahi@gmail.com>
2026-04-07 18:04:58 -0400

    feat(boids): add Old Default preset preserving original controller defaults

--- src/data/boids-presets.ts
+++ src/data/boids-presets.ts
@@ -13,6 +13,31 @@ export interface BoidsPreset {
 
 export const BOIDS_PRESETS: BoidsPreset[] = [
   {
+    "id": "old-default",
+    "name": "Old Default",
+    "isDefault": false,
+    "params": {
+      "dt": 0.016,
+      "numParticles": 200,
+      "attractionRadius": 0.2,
+      "repulsionRadius": 0.05,
+      "attraction": 0.3,
+      "repulsion": 1.5,
+      "alignment": 0.1,
+      "friction": 2,
+      "maxSpeed": 0.22,
+      "mouseRadius": 0.15,
+      "coneAngle": -0.5,
+      "size": 0.02,
+      "shapeId": 0,
+      "colorR": 0.88,
+      "colorG": 0.63,
+      "colorB": 0.25
+    },
+    "trailsEnabled": false,
+    "trailDecay": 0.92
+  },
+  {
     "id": "pink-metal-1775549969719",
     "name": "Pink Metal",
     "params": {
